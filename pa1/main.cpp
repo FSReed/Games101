0211ad97ce02847530361f3e0ee63c39e47ebb90
FSReed <1714747596@qq.com>
2026-01-11 22:10:07 +0800

    feat(Assignment 1): bonus task (arbitrary rotation axis)

--- pa1/main.cpp
+++ pa1/main.cpp
@@ -62,6 +62,25 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     return projection;
 }
 
+Eigen::Matrix4f get_rotation_matrix(Eigen::Vector3f axis, float rotation_angle) {
+    rotation_angle = rotation_angle / 180 * MY_PI;
+    axis.normalize();
+    Eigen::Matrix3f rotation = Eigen::Matrix3f::Identity();
+    Eigen::Matrix3f N;
+    N << 0, -axis[2], axis[1], axis[2], 0, -axis[0], -axis[1], axis[0], 0;
+
+    rotation = std::cos(rotation_angle) * rotation;
+    rotation = rotation + (1 - std::cos(rotation_angle)) * axis * axis.transpose();
+    rotation = rotation + std::sin(rotation_angle) * N;
+
+    // Construct the homogeneous matrix
+    Eigen::Matrix4f result;
+    result.block(0, 0, 3, 3) << rotation;
+    result.col(3) << 0, 0, 0, 1;
+    result.row(3).head(3) << 0, 0, 0;
+    return result;
+}
+
 int main(int argc, const char** argv)
 {
     float angle = 0;
@@ -95,7 +114,7 @@ int main(int argc, const char** argv)
 
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
@@ -106,12 +125,15 @@ int main(int argc, const char** argv)
         return 0;
     }
 
+    // rotation axis
+    Eigen::Vector3f axis(0, 1, 0);
     while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        // r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation_matrix(axis, angle));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
